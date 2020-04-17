5fc5edcfd0b18078ac3fda1520c3e4a128c23d3c
senoz <senozmca@gmail.com>
2020-04-17 15:59:31 +0530

    Update constant.ts

--- src/app/_constants/constant.ts
+++ src/app/_constants/constant.ts
@@ -1,9 +1,9 @@
 export const GlobalVariables = {
     gpayNumber:  9659392919,
-    defaultApartment: 'Embassy Residency',
-    twilioNumber: '+12055768173',
-    accountSid: 'AC87326a2e2c93fa7cc5bbcde8774299a2',
-    authToken: '8b00a9fb1472fb96087bdedf56b33334',
-    whatsappGroup: 'Join Whatsapp Group: https://chat.whatsapp.com/E9GRvR8hSbHHOVnyP8NnLO',
-    whatsappContent: '*Embassy Residency*\n\n 20L Watercan at just *Rs.25\-* by using promocode\n\n *No deposit for the cans*\n\n Share your neigbours/friends in Embassy through app to get more benefits & wallet amount\n\n',
+    defaultApartment: '',
+    twilioNumber: '+',
+    accountSid: '',
+    authToken: '',
+    whatsappGroup: '',
+    whatsappContent: 'Message',
 };
